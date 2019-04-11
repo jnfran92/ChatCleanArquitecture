6e6a29e791c4c8f4862483ccf05a98f897a374e8
Juan Chango <jchango@udlanet.ec>
2019-04-10 20:04:46 -0500

    Unit Test Done!

--- data/src/test/java/com/speakliz/data/repository/datasource/DiskPostDataStoreTest.java
+++ data/src/test/java/com/speakliz/data/repository/datasource/DiskPostDataStoreTest.java
@@ -1,5 +1,6 @@
 package com.speakliz.data.repository.datasource;
 
+import com.speakliz.data.entity.PostEntity;
 import com.speakliz.data.utils.local.LocalApi;
 
 import org.junit.Before;
@@ -8,12 +9,15 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import io.reactivex.Observable;
+
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DiskPostDataStoreTest {
 
-    private static final int FADE_POST_ID = 12;
+    private static final int FAKE_POST_ID = 12;
 
     @Mock private DiskPostDataStore mockDiskPostDataStore;
     @Mock private LocalApi mockLocalApi;
@@ -29,7 +33,16 @@ public class DiskPostDataStoreTest {
         verify(mockLocalApi).postEntityList();
     }
 
-//    @Test
-//    public void postEntityDetails() {
-//    }
+    @Test
+    public void testPostEntityDetails() {
+
+        PostEntity postEntity = new PostEntity();
+        Observable<PostEntity> postEntityObservable = Observable.just(postEntity);
+
+        given(mockDiskPostDataStore.postEntityDetails(FAKE_POST_ID)).willReturn(postEntityObservable);
+
+
+        mockDiskPostDataStore.postEntityDetails(FAKE_POST_ID);
+        verify(mockLocalApi).postEntityById(FAKE_POST_ID);
+    }
 }
