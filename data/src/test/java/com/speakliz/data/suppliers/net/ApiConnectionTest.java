bf98479dbe491d0fa5484d1adfdaddb67302b92c
Juan Chango <jchango@udlanet.ec>
2019-04-24 13:16:45 -0500

    ApiConnection finished and Test created!

---
+++ data/src/test/java/com/speakliz/data/suppliers/net/ApiConnectionTest.java
@@ -0,0 +1,29 @@
+package com.speakliz.data.suppliers.net;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+import static org.junit.Assert.*;
+
+public class ApiConnectionTest {
+
+    private ApiConnection apiConnection;
+
+    private static final String URL_TO_TEST = "https://jsonplaceholder.typicode.com/posts";
+
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        apiConnection = ApiConnection.createGET(URL_TO_TEST);
+    }
+
+    @Test
+    public void testConnection(){
+        String response = apiConnection.requestSyncCall();
+        System.out.println(response);
+
+        assertNotNull(response);
+    }
+}
