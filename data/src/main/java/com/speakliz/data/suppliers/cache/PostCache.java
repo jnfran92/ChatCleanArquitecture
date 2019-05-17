0cae1410d280ae14cacbccbf59a03fd2045e64a0
Juan Chango <jchango@udlanet.ec>
2019-05-17 10:39:04 -0500

    Added Cache interface

---
+++ data/src/main/java/com/speakliz/data/suppliers/cache/PostCache.java
@@ -0,0 +1,49 @@
+package com.speakliz.data.suppliers.cache;
+
+import com.speakliz.data.entity.PostEntity;
+
+import io.reactivex.Observable;
+
+/**
+ * Get and post cached {@link PostEntity} data.
+ */
+
+public interface PostCache {
+
+    /**
+     * Get cached {@link PostEntity}
+     *
+     * @param postId    int PostId
+     * @return @link PostEntity in Observable object
+     */
+    Observable<PostEntity> get(final int postId);
+
+    /**
+     * Cache {@link PostEntity}
+     *
+     * @param postEntity {@link PostEntity}
+     */
+    void put(PostEntity postEntity);
+
+    /**
+     * Verify if is Cached a {@link PostEntity} by passing the postId argument
+     *
+     * @param postId    postId int
+     * @return  boolean true if it is cached or false if it is not.
+     */
+    boolean isCached(final int postId);
+
+    /**
+     * Check if the cache data is expired
+     *
+     * @return true if it is expired, otherwise false.
+     */
+    boolean isExpired();
+
+    /**
+     * Evact all elements of the cache
+     */
+    void evictAll();
+
+
+}
